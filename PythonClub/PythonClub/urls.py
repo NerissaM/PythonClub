2fed51396c051eeeb028fc97fb94bbda0dafefd9
Nerissa Metully <47128742+NerissaM@users.noreply.github.com>
2019-04-11 12:02:11 -0700

    view commit

--- PythonClub/PythonClub/urls.py
+++ PythonClub/PythonClub/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('PythonClub/', include ('PythonClub.urls'))
+    path('ClubApp/', include ('ClubApp.urls'))
 ]
