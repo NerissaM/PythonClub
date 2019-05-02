2da3a4422ad42c47d9ae1016281606fe36b7fafd
Nerissa Metully <47128742+NerissaM@users.noreply.github.com>
2019-05-02 10:25:39 -0700

    commit resource views

--- PythonClub/ClubApp/views.py
+++ PythonClub/ClubApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinute, Resource, Event
 
 # Create your views here.
 def index (request):
     return render(request, 'ClubApp/index.html')
+
+def getTypes(request):
+    type_list=Resource.objects.all()
+    return render(request, 'ClubApp/types.html', {'type_list' : type_list})
